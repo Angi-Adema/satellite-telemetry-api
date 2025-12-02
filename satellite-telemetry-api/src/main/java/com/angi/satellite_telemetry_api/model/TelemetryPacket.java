9793479024154cf578558041b12d5b02aba0028a
Angi-Adema <dr.adema@gmail.com>
2025-12-02 12:03:32 -0700

    added the logic to the model TelemetryPacket.java to create the table for storing telemetry data in

--- satellite-telemetry-api/src/main/java/com/angi/satellite_telemetry_api/model/TelemetryPacket.java
+++ satellite-telemetry-api/src/main/java/com/angi/satellite_telemetry_api/model/TelemetryPacket.java
@@ -1,5 +1,35 @@
 package com.angi.satellite_telemetry_api.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class TelemetryPacket {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String satelliteId;
+
+    private Instant timestamp;
+
+    private double batteryLevel;
+
+    private double temperature;
+
+    // Tells JPA to include in the same row the roll, pitch and yaw fields instead of creating a new table.
+    @Embedded
+    private Orientation orientation;
+
+    // Store the status as a string (NOMINAL, WARNING, CRITICAL) in the database.
+    @Enumerated(EnumType.STRING)
+    private TelemetryStatus status;
 }
