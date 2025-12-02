5a586e2ce45aeca60857b784d35d453c65490503
Angi-Adema <dr.adema@gmail.com>
2025-12-02 12:18:27 -0700

    added the logic to Orientation.java to outline the data to be saved to the parent table in TelemetryPacket.java

--- satellite-telemetry-api/src/main/java/com/angi/satellite_telemetry_api/model/Orientation.java
+++ satellite-telemetry-api/src/main/java/com/angi/satellite_telemetry_api/model/Orientation.java
@@ -1,5 +1,19 @@
 package com.angi.satellite_telemetry_api.model;
 
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Orientation {
-    
+
+    private double roll;
+    private double pitch;
+    private double yaw;
 }
+
+// Attitude is the orientation of the satellite in 3D space, typically represented by roll, pitch, and yaw angles.
