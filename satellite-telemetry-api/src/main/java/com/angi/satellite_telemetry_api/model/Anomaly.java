d6d895a5774cc2d18a63798fd124de0f07f28f34
Angi-Adema <dr.adema@gmail.com>
2025-12-02 12:30:44 -0700

    added the logic to Anomaly.java to hold the structure of the reported data from the anomaly if occurs.

--- satellite-telemetry-api/src/main/java/com/angi/satellite_telemetry_api/model/Anomaly.java
+++ satellite-telemetry-api/src/main/java/com/angi/satellite_telemetry_api/model/Anomaly.java
@@ -1,5 +1,27 @@
+// This file serves as a DTO (data transfer object) that represents a detected problem in the telemetry stream.
 package com.angi.satellite_telemetry_api.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Anomaly {
-    
+    private String satelliteId;
+
+    private Instant timestamp;
+
+    // Type of anomaly (LOW_BATTERY, HIGH_TEMPERATURE, CRITICAL_STATE, etc.)
+    private String type;
+
+    // Battery level at the time of the anomaly (if applicable)
+    private Double batteryLevel;
+
+    // Temperature at the time of the anomaly (if applicable)
+    private Double temperature;
+
 }
