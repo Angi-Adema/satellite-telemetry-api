31b5fc5b1156468f8a976a28729c147390e66a57
Angi-Adema <dr.adema@gmail.com>
2025-12-02 11:22:08 -0700

    first commit

---
+++ satellite-telemetry-api/src/main/java/com/angi/satellite_telemetry_api/SatelliteTelemetryApiApplication.java
@@ -0,0 +1,13 @@
+package com.angi.satellite_telemetry_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SatelliteTelemetryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SatelliteTelemetryApiApplication.class, args);
+	}
+
+}
---
+++ satellite-telemetry-api/src/test/java/com/angi/satellite_telemetry_api/SatelliteTelemetryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.angi.satellite_telemetry_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SatelliteTelemetryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
